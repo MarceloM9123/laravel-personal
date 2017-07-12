6818167a1eeef99a0bf8e004f9d8da88e8084006
Marcelo <marcelomata91@gmail.com>
2017-07-12 16:59:33 -0500

    fixed navbar menu bug

--- resources/views/blog/layout.blade.php
+++ resources/views/blog/layout.blade.php
@@ -95,9 +95,7 @@
 
     @yield('content')
 
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
-<!-- Use downloaded version of Bootstrap -->
-<script src="js/js/bootstrap.min.js"></script>
+@include('blog.footer')
 
 </body>
 </html>
